161df74ddb53475531a9f056f6018e69e0b70dbe
will hunting <willhunting@wills-Mac.local>
2014-05-28 21:56:55 +0800

    Initial Commit

---
+++ SuperCard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by will hunting on 5/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
